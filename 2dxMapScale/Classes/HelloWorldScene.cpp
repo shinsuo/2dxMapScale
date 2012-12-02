d5e48357a5a1874fa7edb824ca5b0d766e683810
shin <funny.suo@gmail.com>
2012-12-02 22:32:18 +0800

    add Project

--- 2dxMapScale/Classes/HelloWorldScene.cpp
+++ 2dxMapScale/Classes/HelloWorldScene.cpp
@@ -4,6 +4,9 @@
 using namespace cocos2d;
 using namespace CocosDenshion;
 
+static float MINSCALE = 0.5;
+static float MAXSCALE = 2;
+
 CCScene* HelloWorld::scene()
 {
     // 'scene' is an autorelease object
@@ -28,7 +31,7 @@ bool HelloWorld::init()
     {
         return false;
     }
-
+    /*
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
     //    you may modify it.
@@ -70,9 +73,112 @@ bool HelloWorld::init()
 
     // add the sprite as a child to this layer
     this->addChild(pSprite, 0);
+    //*/
+    this->setTouchEnabled(1);
+    
+    m_pTileMap = CCTMXTiledMap::create("tilemap.tmx");
+    this->addChild(m_pTileMap);
+    CCLog("pos:%f,%f,anchor:%f,%f",m_pTileMap->getPositionX(),m_pTileMap->getPositionY(),m_pTileMap->getAnchorPoint().x,m_pTileMap->getAnchorPoint().y);
+    CCLog("%f,%f--",m_pTileMap->getMapSize().width*m_pTileMap->getTileSize().width,m_pTileMap->getMapSize().height*m_pTileMap->getTileSize().height);
+//    m_pTileMap->setPosition(0, 0);
+//    m_pTileMap->setAnchorPoint(CCPointZero);
+    m_WinSize = CCDirector::sharedDirector()->getWinSize();
+    
+    
     
     return true;
 }
+
+#pragma mark Touch
+void HelloWorld::ccTouchesBegan(CCSet *pTouches, CCEvent *pEvent)
+{
+    
+    
+    switch (pTouches->count()) {
+        case 1:
+            this->m_LastPoint = ((CCTouch *)pTouches->anyObject())->getLocation();
+            break;
+        case 2:
+        {
+            this->m_LastScale = m_pTileMap->getScale();
+            
+            CCSetIterator iter;
+            CCPoint pt[2];
+            int i = 0;
+            for (iter = pTouches->begin(); iter != pTouches->end(); iter++) {
+                CCTouch *touch = (CCTouch *)*iter;
+                pt[i++] = touch->getLocation();
+            }
+            
+            this->m_BeganDistance = ccpDistance(pt[0],pt[1]);
+        }
+            
+            break;
+        default:
+            break;
+    }
+}
+
+void HelloWorld::ccTouchesMoved(CCSet *pTouches, CCEvent *pEvent)
+{
+    switch (pTouches->count()) {
+        case 1:
+        {
+            CCPoint curPoint = ((CCTouch *)pTouches->anyObject())->getLocation();
+            CCPoint translation = ccpSub(this->m_LastPoint,curPoint);
+            CCPoint newPos = ccpSub(m_pTileMap->getPosition(), translation);
+            if (this->isAllowMove(newPos)) {
+                m_pTileMap->setPosition(newPos);
+            }
+            
+            
+        }
+            break;
+        case 2:
+        {
+            CCSetIterator iter;
+            CCPoint pt[2];
+            int i = 0;
+            for (iter = pTouches->begin(); iter != pTouches->end(); iter++) {
+                CCTouch *touch = (CCTouch *)*iter;
+                pt[i++] = touch->getLocation();
+            }
+            
+            this->m_CurDistance = ccpDistance(pt[0],pt[1]);
+            
+            float changedScale = this->m_CurDistance/this->m_BeganDistance;
+            float nowScale = this->m_LastScale - 1 + changedScale;
+            nowScale = MIN(nowScale,MINSCALE);
+            nowScale = MAX(nowScale,MAXSCALE);
+            
+            if (this->m_LastScale > nowScale) {
+                CCPoint newPos = ccpSub(m_pTileMap->getPosition(), ccpMult(ccpNormalize(m_pTileMap->getPosition()), ccpLength(m_pTileMap->getPosition())*(this->m_LastScale - nowScale)/(this->m_LastScale -1)));
+                if (this->isAllowMove(newPos)) {
+                    m_pTileMap->setPosition(newPos);
+                }
+            }
+            m_pTileMap->setScale(nowScale);
+            
+        }
+            break;
+        default:
+            break;
+    }
+}
+
+bool HelloWorld::isAllowMove(const CCPoint &pt)
+{
+    float x = m_WinSize.width - m_pTileMap->getTileSize().width*m_pTileMap->getMapSize().width*m_pTileMap->getScale();
+    float y = m_WinSize.height - m_pTileMap->getTileSize().height*m_pTileMap->getMapSize().height*m_pTileMap->getScale();
+    float width = abs(x);
+    float height = abs(y);
+    
+    
+    CCRect rect = CCRectMake(x, y, width, height);
+    return (pt.x > rect.origin.x && pt.x < rect.origin.x+rect.size.width &&
+            pt.y > rect.origin.y && pt.y < rect.origin.y+rect.size.height);
+}
+
 
 void HelloWorld::menuCloseCallback(CCObject* pSender)
 {
--- 2dxMapScale/Classes/HelloWorldScene.h
+++ 2dxMapScale/Classes/HelloWorldScene.h
@@ -8,6 +8,11 @@ class HelloWorld : public cocos2d::CCLayer
 public:
     // Method 'init' in cocos2d-x returns bool, instead of 'id' in cocos2d-iphone (an object pointer)
     virtual bool init();
+    
+    virtual void ccTouchesBegan(cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
+    virtual void ccTouchesMoved(cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
+//    virtual void ccTouchesEnded(cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
+//    virtual void ccTouchesCancelled(cocos2d::CCSet *pTouches, cocos2d::CCEvent *pEvent);
 
     // there's no 'id' in cpp, so we recommend to return the class instance pointer
     static cocos2d::CCScene* scene();
@@ -17,6 +22,19 @@ public:
 
     // preprocessor macro for "static create()" constructor ( node() deprecated )
     CREATE_FUNC(HelloWorld);
+    
+private:
+    cocos2d::CCTMXTiledMap *m_pTileMap;
+    
+    float m_CurDistance;
+    float m_BeganDistance;
+    float m_LastScale;
+    cocos2d::CCPoint m_LastPoint;
+    
+    cocos2d::CCSize m_WinSize;
+    
+    cocos2d::CCSize getMapSize();
+    bool isAllowMove(const cocos2d::CCPoint &pt);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
