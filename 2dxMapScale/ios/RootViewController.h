a5eeaa255a65ce67e300c8a8be61230a93291b67
shin <shin@shins-MacBook-Pro.local>
2012-12-02 18:55:48 +0800

    Initial Commit

---
+++ 2dxMapScale/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  _dxMapScaleAppController.h
+//  2dxMapScale
+//
+//  Created by shin on 12/2/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
